e991caf22571e4b91de7e65e12041c571cd7c1ea
b_liu <b.liu@nki.nl>
2016-04-18 23:12:18 +0200

    add compare plasmid map function for plasmids

--- ecloning/ecloning/Controllers/PlasmidController.cs
+++ ecloning/ecloning/Controllers/PlasmidController.cs
@@ -114,7 +114,7 @@ namespace ecloning.Controllers
             ViewBag.Plasmids = JsonConvert.SerializeObject(plasmids.ToList());
             var plasmidId = plasmids.Select(p => p.id).ToList();
             //pass all features into json
-            var features = db.plasmid_map.Where(f => f.feature_id != 4).OrderBy(p => p.plasmid_id).OrderBy(s => s.start).Where(p=>plasmidId.Contains(p.plasmid_id)).Select(f => new { show_feature = f.show_feature, end = f.end, feature = f.common_feature != null ? f.common_feature.label : f.feature, type_id = f.feature_id, start = f.start, cut = f.cut, clockwise = f.clockwise == 1 ? true : false });
+            var features = db.plasmid_map.Include(p => p.plasmid).Where(f => f.feature_id != 4).OrderBy(p => p.plasmid_id).OrderBy(s => s.start).Where(p=>plasmidId.Contains(p.plasmid_id)).Select(f => new { pId = f.plasmid.id, pName = f.plasmid.name, pSeqCount = f.plasmid.seq_length, show_feature = f.show_feature, end = f.end, feature = f.common_feature != null ? f.common_feature.label : f.feature, type_id = f.feature_id, start = f.start, cut = f.cut, clockwise = f.clockwise == 1 ? true : false });
             ViewBag.Features = JsonConvert.SerializeObject(features.ToList());
             return View();
         }
