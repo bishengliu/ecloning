ec1ebbf7aebaca652d9950d8ec9fc733b7a4e74a
b_liu <b.liu@nki.nl>
2016-07-04 23:56:24 +0200

    finish with adding dpart and groups

--- ecloning/ecloning/Areas/Admin/Controllers/UsersController.cs
+++ ecloning/ecloning/Areas/Admin/Controllers/UsersController.cs
@@ -135,22 +135,38 @@ namespace ecloning.Areas.Admin.Controllers
             {
                 return RedirectToAction("Index");
             }
-            dpart.name = dpName;
-            dpart.des = dpName;
-            db.SaveChanges();
-            return RedirectToAction("Index");
+            try
+            {
+                dpart.name = dpName;
+                dpart.des = dpName;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Index");
+            }
+            
         }
 
         [Authorize(Roles = "appAdmin, InstAdmin")]
         [HttpPost]
         public ActionResult AddDepart(string dpName)
         {
-            var dpart = new department();
-            dpart.name = dpName;
-            dpart.des = dpName;
-            db.departments.Add(dpart);
-            db.SaveChanges();
-            return RedirectToAction("Index");
+            try
+            {
+                var dpart = new department();
+                dpart.name = dpName;
+                dpart.des = dpName;
+                db.departments.Add(dpart);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Index");
+            }
+
         }
 
         [Authorize(Roles = "appAdmin, InstAdmin")]
@@ -174,12 +190,18 @@ namespace ecloning.Areas.Admin.Controllers
             {
                 return RedirectToAction("Index");
             }
-
-            db.departments.Remove(dpart);
-            db.SaveChanges();
-            return RedirectToAction("Index");
+            try
+            {
+                db.departments.Remove(dpart);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch(Exception)
+            {
+                return RedirectToAction("Index");
+            }
+            
         }
-
 
         [Authorize(Roles = "appAdmin, InstAdmin")]
         [HttpPost]
@@ -246,7 +268,7 @@ namespace ecloning.Areas.Admin.Controllers
 
         [Authorize(Roles = "appAdmin, InstAdmin")]
         [HttpPost]
-        public ActionResult AddGroup(int? id, string gpName, string gpEmail, string gpDes)
+        public ActionResult AddGroup(int? id, string gpName, string gpEmail)
         {
             if (id == null)
             {
@@ -258,17 +280,206 @@ namespace ecloning.Areas.Admin.Controllers
             {
                 return RedirectToAction("Index");
             }
-            var group = new group();
-            group.depart_id = (int)id;
-            group.name = gpName;
-            group.email = gpEmail;
-            group.des = gpDes;
-            //genCode
-            var gencode = new genCode();
-            group.code = gencode.HashStringCode(256);
-            db.groups.Add(group);
-            db.SaveChanges();
-            return RedirectToAction("Index");
+            try
+            {
+                var group = new group();
+                group.depart_id = (int)id;
+                group.name = gpName;
+                group.email = gpEmail;
+                group.des = gpName;
+                //genCode
+                var gencode = new genCode();
+                group.code = gencode.HashStringCode(256);
+                db.groups.Add(group);
+                db.SaveChanges();
+
+                //send email for registration
+                var AdminEmail = User.Identity.GetUserName();
+                var AdminName = db.people.Where(e => e.email == AdminEmail).FirstOrDefault().first_name + " " + db.people.Where(e => e.email == AdminEmail).FirstOrDefault().last_name;
+                var html = "I've just created your group on the website: <a href=\"" + eCloningSettings.AppURI + "\">" + eCloningSettings.AppURI + "</a>";
+                html = html + "<br/><p>Please copy the following code to register in your group</p>";
+                html = html + "<p><strong>" + group.code + "</strong></p>";
+                html = html + "<br/><br/><p>Regards, <br/>" + AdminName + "<br/>" + AdminEmail + "</p>";
+                if (eCloningSettings.AppHosting == "Cloud")
+                {
+                    //send email using send grid
+                    var msg = new SendGridMessage();
+                    msg.From = new MailAddress(AdminEmail, AdminName);
+                    msg.AddTo(group.email);
+                    msg.Subject = "Your Group Created";
+
+                    msg.Html = html;
+                    var username = eCloningSettings.SendgridLoginName;
+                    var pswd = eCloningSettings.SendgridPsw;
+                    var credentials = new NetworkCredential(username, pswd);
+
+                    // Create an Web transport for sending email.
+                    var transportWeb = new Web(credentials);
+                    // Send the email.
+                    // You can also use the **DeliverAsync** method, which returns an awaitable task.
+                    transportWeb.DeliverAsync(msg);
+                }
+                if (eCloningSettings.AppHosting == "Local")
+                {
+                    //send email using local smtp
+                    var message = new MailMessage();
+                    message.To.Add(group.email);  // replace with valid value 
+                    message.From = new MailAddress(AdminEmail);  // replace with valid value
+                    message.Subject = "Your Group Created";
+                    message.Body = html;
+                    message.IsBodyHtml = true;
+
+                    using (var smtp = new SmtpClient())
+                    {
+                        var credential = new NetworkCredential
+                        {
+                            UserName = LocalSMTP.Login,  // replace with valid value
+                            Password = LocalSMTP.Password  // replace with valid value
+                        };
+                        smtp.Credentials = credential;
+                        smtp.Host = LocalSMTP.Server;
+                        smtp.Port = LocalSMTP.Port;
+                        smtp.EnableSsl = LocalSMTP.EnableSsl;
+                        smtp.SendMailAsync(message);
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        [Authorize(Roles = "appAdmin, InstAdmin")]
+        [HttpGet]
+        public ActionResult SendCode(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            //find the group
+            var group = db.groups.Find(id);
+            if (group == null)
+            {
+                return RedirectToAction("Index");
+            }
+            try
+            {
+                var code = group.code;
+                //send the code
+                //send email for registration
+                var AdminEmail = User.Identity.GetUserName();
+                var AdminName = db.people.Where(e => e.email == AdminEmail).FirstOrDefault().first_name + " " + db.people.Where(e => e.email == AdminEmail).FirstOrDefault().last_name;
+                var html = "Below is the code to register on the website: <a href=\"" + eCloningSettings.AppURI + "\">" + eCloningSettings.AppURI + "</a>";
+                html = html + "<br/><p>Please copy the following code to register in your group</p>";
+                html = html + "<p><strong>" + group.code + "</strong></p>";
+                html = html + "<br/><br/><p>Regards, <br/>" + AdminName + "<br/>" + AdminEmail + "</p>";
+                if (eCloningSettings.AppHosting == "Cloud")
+                {
+                    //send email using send grid
+                    var msg = new SendGridMessage();
+                    msg.From = new MailAddress(AdminEmail, AdminName);
+                    msg.AddTo(group.email);
+                    msg.Subject = "Registration Code";
+
+                    msg.Html = html;
+                    var username = eCloningSettings.SendgridLoginName;
+                    var pswd = eCloningSettings.SendgridPsw;
+                    var credentials = new NetworkCredential(username, pswd);
+
+                    // Create an Web transport for sending email.
+                    var transportWeb = new Web(credentials);
+                    // Send the email.
+                    // You can also use the **DeliverAsync** method, which returns an awaitable task.
+                    transportWeb.DeliverAsync(msg);
+                }
+                if (eCloningSettings.AppHosting == "Local")
+                {
+                    //send email using local smtp
+                    var message = new MailMessage();
+                    message.To.Add(group.email);  // replace with valid value 
+                    message.From = new MailAddress(AdminEmail);  // replace with valid value
+                    message.Subject = "Registration Code";
+                    message.Body = html;
+                    message.IsBodyHtml = true;
+
+                    using (var smtp = new SmtpClient())
+                    {
+                        var credential = new NetworkCredential
+                        {
+                            UserName = LocalSMTP.Login,  // replace with valid value
+                            Password = LocalSMTP.Password  // replace with valid value
+                        };
+                        smtp.Credentials = credential;
+                        smtp.Host = LocalSMTP.Server;
+                        smtp.Port = LocalSMTP.Port;
+                        smtp.EnableSsl = LocalSMTP.EnableSsl;
+                        smtp.SendMailAsync(message);
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        [Authorize(Roles = "appAdmin, InstAdmin")]
+        [HttpPost]
+        public ActionResult EditGroup(int? id, string gpName, string gpEmail)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            //find the group
+            var group = db.groups.Find(id);
+            if (group == null)
+            {
+                return RedirectToAction("Index");
+            }
+            try
+            {
+                group.name = gpName;
+                group.email = gpEmail;
+                group.des = gpName;                
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        [Authorize(Roles = "appAdmin, InstAdmin")]
+        [HttpGet]
+        public ActionResult DeleteGroup(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            //find the group
+            var group = db.groups.Find(id);
+            if (group == null)
+            {
+                return RedirectToAction("Index");
+            }
+            try
+            {
+                db.groups.Remove(group);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Index");
+            }
         }
     }
 }
