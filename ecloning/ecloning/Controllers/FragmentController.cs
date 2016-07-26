37c4eb9e463d7a274be833dd74851433b02b344a
Bisheng Liu <b.liu@nki.nl>
2016-07-26 15:10:49 +0200

    finish with adding fragments

--- ecloning/ecloning/Controllers/FragmentController.cs
+++ ecloning/ecloning/Controllers/FragmentController.cs
@@ -154,29 +154,81 @@ namespace ecloning.Controllers
                 var share = db.group_shared.Where(g => groupId.Contains(g.group_id) || adminId.Contains(g.group_id)).Where(c => c.category == "fragment").OrderByDescending(r => r.resource_id);
                 shareIds = share.Select(r => r.resource_id).ToList();
             }
-            var shareFragments = db.fragments.Where(f => shareIds.Contains(f.id)).ToList();
-            ViewBag.shareFragments = shareFragments;
+
             //get my vectors and ally my own plasmid derived plasmid
             var myFragments = db.fragments.Where(f => f.people_id == userInfo.PersonId).Where(f => !shareIds.Contains(f.id));
             List<int> myfragId = myFragments.Select(f => f.id).ToList();
 
             //get the combined ids for get the fragment map
             List<int> combinedId = shareIds.Concat(myfragId).ToList();
-            var maps = db.fragment_map.Where(m => combinedId.Contains(m.fragment_id)).OrderBy(f => f.fragment_id).Select(f => 
-            new {
-                fId = f.fragment_id,
-                fName = f.fragment.name,
-                show_feature = f.show_feature,
-                end = f.end,
-                feature = f.feature,
-                type_id = f.feature_id,
-                start = f.start,
-                cut = f.cut,
-                clockwise = f.clockwise == 1 ? true : false
-            });
-            ViewBag.Features = JsonConvert.SerializeObject(maps.ToList());
 
-            return View(myFragments.ToList());
+            IList<FragmentViewModel> fragments = new List<FragmentViewModel>();
+            var allFragments = db.fragments.Where(f => combinedId.Contains(f.id)).OrderByDescending(f => f.name).ToList();
+            if (allFragments.Count() > 0)
+            {
+                foreach (var item in allFragments)
+                {
+
+                    //convert enzyme id into enzyme names
+                    var enzymeIds = new List<int>();
+                    var enzymes = new List<string>();
+                    if (item.enzyme_id != null)
+                    {
+                        var enzymeIdStringArray = item.enzyme_id.Split(',');
+                        enzymeIds.Add(Int32.Parse(enzymeIdStringArray[0]));
+                        enzymeIds.Add(Int32.Parse(enzymeIdStringArray[1]));
+                        var enzyme1 = db.restri_enzyme.Find(enzymeIds[0]).name;
+                        enzymes.Add(enzyme1);
+                        var enzyme2 = db.restri_enzyme.Find(enzymeIds[1]).name;
+                        enzymes.Add(enzyme2);
+                    }
+
+                    //find the features
+                    IList<fragmentFeatures> featuresArray = new List<fragmentFeatures>();
+                    var features = db.fragment_map.Where(f => f.fragment_id == item.id).Where(f=>f.feature_id != 4).OrderBy(s => s.start);
+                    if (features.Count() > 0)
+                    {
+                        foreach (var feature in features)
+                        {
+                            var fObj = new fragmentFeatures();
+                            fObj.clockwise = feature.clockwise == 1 ? true : false;
+                            fObj.cut = feature.cut;
+                            fObj.common_id = feature.common_id;
+                            fObj.end = feature.end;
+                            fObj.feature = feature.feature;
+                            fObj.show_feature = feature.show_feature;
+                            fObj.start = feature.start;
+                            fObj.type_id = feature.feature_id;
+                            featuresArray.Add(fObj);
+                        }
+                    }
+                    //get plasmid name
+                    var plasmid = db.plasmids.Find((int)item.plasmid_id);
+
+                    var fragment = new FragmentViewModel();
+                    fragment.featureArray = featuresArray;
+                    fragment.id = item.id;
+                    fragment.fName = item.plasmid_id != null ? plasmid.name: item.name;
+                    fragment.plasmid_id = (int)item.plasmid_id;
+                    fragment.enzymes = enzymes;
+                    fragment.f_start = (int)item.forward_start;
+                    fragment.f_end = (int)item.forward_end;
+                    fragment.fSeq = item.forward_seq;
+                    //overhangs
+                    var overhangs = new List<int>();
+                    overhangs.Add((int)item.rc_left_overhand);
+                    overhangs.Add((int)item.rc_right_overhand);
+                    fragment.overhangs = overhangs;
+                    fragment.cSeq = item.rc_seq;
+                    fragments.Add(fragment); //add fragments
+
+                }
+            }
+            ViewBag.shareIds = shareIds;
+            ViewBag.FragmentIds = combinedId;
+            ViewBag.Fragments = JsonConvert.SerializeObject(fragments.ToList());
+
+            return View();
         }
 
         //add linearized vectors
@@ -408,7 +460,7 @@ namespace ecloning.Controllers
         //delete linearized vectors
         [Authorize]
         [HttpGet]
-        public ActionResult Delete(int? id)
+        public ActionResult DVector(int? id)
         {
             //id is the fragment id
             if (id == null)
@@ -420,16 +472,7 @@ namespace ecloning.Controllers
             {
                 return HttpNotFound();
             }
-            return View(fragment);
-            
-        }
 
-        [HttpPost, ActionName("Delete")]
-        [Authorize]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            var fragment = db.fragments.Find(id);
             db.fragments.Remove(fragment);
 
             //detele all the reffered features in fragment_map
@@ -443,7 +486,9 @@ namespace ecloning.Controllers
             }
             db.SaveChanges();
             return RedirectToAction("Fragment", "Fragment");
+            
         }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
