98106c937213f38ea5f7a59d0c2d7a722668fb2a
Bisheng Liu <b.liu@nki.nl>
2016-07-27 16:24:49 +0200

    working on adding exp

---
+++ ecloning/ecloning/Controllers/ExperimentController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using ecloning.Models;
+
+namespace ecloning.Controllers
+{
+    public class ExperimentController : Controller
+    {
+        private ecloningEntities db = new ecloningEntities();
+        // GET: Experiment
+        [Authorize]
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var userInfo = new UserInfo(userId);
+            var groupInfo = new GroupInfo(userInfo.PersonId);
+            ViewBag.PersonId = userInfo.PersonId;
+            //show shared experiment
+            var sharedExps= db.group_shared.Where(p => p.category == "experiment").Where(g => groupInfo.groupId.Contains(g.group_id)).OrderByDescending(r=>r.resource_id);
+            List<int> sharedIds = new List<int>();
+            if (sharedExps.Count() > 0)
+            {
+                sharedIds = sharedExps.Select(r => r.resource_id).ToList();
+            }
+            ViewBag.SharedId = sharedIds;
+            //show my experiment
+            var myExpIds = new List<int>();
+            var myExps = db.experiments.Where(e => e.people_id == userInfo.PersonId).Where(e => !sharedIds.Contains(e.id)).OrderByDescending(e => e.id);
+            if (myExps.Count() > 0)
+            {
+                myExpIds = myExps.Select(e => e.id).ToList();
+            }
+            //combined id
+            List<int> combinedIds = myExpIds.Concat(sharedIds).ToList();            
+            //get all exps
+            var exps = db.experiments.Where(e => combinedIds.Contains(e.id)).ToList();
+            return View(exps);
+        }
+
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult Create()
+        {
+            //generate list
+            ViewBag.types = new SelectList(db.exp_type.OrderBy(g => g.name), "id", "name");
+            return View();
+        }
+    }
+}
---
+++ ecloning/ecloning/Models/ExperimentViewModal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ecloning.Models
+{
+    public class ExperimentViewModal
+    {
+        //experiment general info
+        public int id { get; set; }
+        [Required(ErrorMessage = "Required")]
+        public string name { get; set; }
+        public int types { get; set; }
+        public string des { get; set; }
+        public Nullable<System.DateTime> dt { get; set; }
+        public List<ExpStep> Steps { get; set; }
+        //steps
+    }
+
+    public class ExpStep
+    {
+        ////save to exp_step table
+        public int id { get; set; }
+        [Required(ErrorMessage = "Required")]
+        public string name { get; set; }
+        [Required(ErrorMessage = "Required")]
+        public int type_id { get; set; }
+        [Required(ErrorMessage = "Required")]
+        public int step_id { get; set; }
+        [Required(ErrorMessage = "Required")]
+        public int people_id { get; set; }
+        public int protocol_id { get; set; }
+        public string des { get; set; } //save to exp_step table
+        public Nullable<System.DateTime> dt { get; set; } //save to exp_step table
+
+
+        //save to exp_step_material table
+        public int forward_primer_id { get; set; }
+        public int reverse_primer_id { get; set; }
+        public int probe_id { get; set; }
+        public string emzyme_id { get; set; }
+        public int plasmid_id { get; set; }
+        public int frag1_id { get; set; }
+        public int frag2_id { get; set; }
+    }
+}
